bb856287bf05ac0e16941f5254eeca9670c2db87
Vijay Vaddem <vijay.vaddem@gmail.com>
2020-09-28 10:34:57 +0530

    Added Todo components. yet to make them work

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { hot } from "react-hot-loader";
+
 import "./App.css";
+import TodoList from "./todos/TodoList";
 
 const App = () => {
   return (
     <div className="App">
       <h1>Todo App with React Redux Thunk</h1>
+      <TodoList />
     </div>
   );
 };
---
+++ src/todos/NewTodoForm.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./NewTodoForm.css";
+
+const NewTodoForm = () => {
+  const [inputValue, setInputValue] = useState("");
+
+  return (
+    <div className="new-todo-form">
+      <input
+        type="text"
+        className="new-todo-input"
+        value={inputValue}
+        placeholder="Add some text"
+        onChange={(e) => setInputValue(e.target.value)}
+      ></input>
+      <button className="new-todo-button">Create</button>
+    </div>
+  );
+};
+
+export default NewTodoForm;
