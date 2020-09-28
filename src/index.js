d9eecca22f71f4cf89fa0e728308bc9665134e94
Vijay Vaddem <vijay.vaddem@gmail.com>
2020-09-29 00:48:36 +0530

    Added redux store and action types

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 
+import { configureStore } from "./redux/store";
 import App from "./App.js";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={configureStore()}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
---
+++ src/redux/actions.js
@@ -0,0 +1,8 @@
+export const CREATE_TODO = "CREATE_TODO";
+
+export const createTodo = (text) => ({
+  type: CREATE_TODO,
+  payload: {
+    text,
+  },
+});
