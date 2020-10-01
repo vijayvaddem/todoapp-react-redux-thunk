26f1c6052d79685094d038160e21cfca28d49457
Vijay Vaddem <vijay.vaddem@gmail.com>
2020-10-01 13:26:08 +0530

    adding persistent layer to persist in local storage

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { persistStore } from "redux-persist";
+import { PersistGate, persistGate } from "redux-persist/lib/integration/react";
 
 import { configureStore } from "./redux/store";
 import App from "./App.js";
 
+const store = configureStore();
+const persistor = persistStore(store);
+
 ReactDOM.render(
-  <Provider store={configureStore()}>
-    <App />
+  <Provider store={store}>
+    <PersistGate loading={<div>loading...</div>} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,22 @@
 import { createStore, combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
+
 import { todos } from "./reducers";
 
 const reducers = { todos };
 
+const persistConfig = {
+  key: "root",
+  storage,
+  stateReconciler: autoMergeLevel2,
+};
 const rootReducers = combineReducers(reducers);
+const persistedReducer = persistReducer(persistConfig, rootReducers);
 
-export const configureStore = () => createStore(rootReducers);
+export const configureStore = () =>
+  createStore(
+    persistedReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
